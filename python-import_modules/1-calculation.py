8244cffa6ba2259e8efd5dc59c9b21b73b2f4f38
Sai HU <laura.sai.hu@gmail.com>
2026-04-26 13:58:23 +0000

    Add calculator_1 module and 1-calculation script for arithmetic operations

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Import functions from calculator_1 and perform operations."""
+
+from calculator_1 import add, sub, mul, div
+
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
---
+++ python-import_modules/calculator_1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines basic arithmetic operations."""
+
+
+def add(a, b):
+    """Return the addition of a and b."""
+    return a + b
+
+
+def sub(a, b):
+    """Return the subtraction of b from a."""
+    return a - b
+
+
+def mul(a, b):
+    """Return the multiplication of a and b."""
+    return a * b
+
+
+def div(a, b):
+    """Return the division of a by b."""
+    return a / b
