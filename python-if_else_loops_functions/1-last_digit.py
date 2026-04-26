af2cbbe0f1a53ec4ad580231f9b34dcaac462e87
Sai HU <laura.sai.hu@gmail.com>
2026-04-26 11:58:41 +0000

    Fix syntax error in split print statement

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -14,5 +14,4 @@ elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
     print(f"Last digit of {number} is {last_digit} "
-          "and is less than 6 and not 0")
-          
+          "and is less than 6 and not 0")       
