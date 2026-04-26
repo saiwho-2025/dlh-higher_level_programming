b50b368e0740914921bab11066b4a21122c9200d
Sai HU <laura.sai.hu@gmail.com>
2026-04-26 15:28:18 +0000

    Fix alternating case order for reverse alphabet

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(25, -1, -1):
-    print("{}".format(chr(122 - i) if i % 2 == 0 else chr(90 - i)), end="")
+    print("{}".format(chr(97 + i) if i % 2 else chr(65 + i)), end="")
