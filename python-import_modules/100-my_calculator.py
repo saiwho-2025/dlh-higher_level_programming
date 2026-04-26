dd45481623f5c257c6f641400f1e23dcf3ca436a
Sai HU <laura.sai.hu@gmail.com>
2026-04-26 15:45:26 +0000

    Add calculator script handling basic operations

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Simple calculator using functions from calculator_1."""
+
+from calculator_1 import add, sub, mul, div
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if op == "+":
+        result = add(a, b)
+    elif op == "-":
+        result = sub(a, b)
+    elif op == "*":
+        result = mul(a, b)
+    elif op == "/":
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    print("{} {} {} = {}".format(a, op, b, result))
