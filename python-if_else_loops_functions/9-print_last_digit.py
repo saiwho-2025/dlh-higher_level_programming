3e7b9fc72cdb632376de6d376b68af5279e61267
Sai HU <laura.sai.hu@gmail.com>
2026-04-26 12:55:16 +0000

    Add function to print last digit of number

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    last = number % 10
+    if number < 0 and last != 0:
+        last -= 10
+    print("{}".format(abs(last)), end="")
+    return abs(last)
