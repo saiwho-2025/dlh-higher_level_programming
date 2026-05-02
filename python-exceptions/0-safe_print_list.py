290da57d121a5e207446e865f18cbccfe96b816d
Sai HU <laura.sai.hu@gmail.com>
2026-05-02 15:37:51 +0000

    edit except in 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,6 +5,6 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i],end="")
             count += 1
-        except Typeerror:
+        except IndexError:
             break
     print("nb_print:", n)
