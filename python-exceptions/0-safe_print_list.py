704a766da2c85ce1360cf5fb38807e44cdbbf448
Sai HU <laura.sai.hu@gmail.com>
2026-05-02 15:27:00 +0000

    print in one line for 0-saft_print.list.py ls git status

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,8 +5,6 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i],end="")
             count += 1
-        except IndexError:
+        except:
             break
-    print()
-    return count
-    
+    print("printed:", n)
