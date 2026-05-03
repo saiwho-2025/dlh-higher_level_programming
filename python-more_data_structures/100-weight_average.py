a1d6158ab0c55c39ad0725c522bcbec3df921770
Sai HU <laura.sai.hu@gmail.com>
2026-05-03 19:19:52 +0000

    Add weight_average function

---
+++ python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    total = 0
+    weight_sum = 0
+
+    for score, weight in my_list:
+        total += score * weight
+        weight_sum += weight
+
+    return total / weight_sum
