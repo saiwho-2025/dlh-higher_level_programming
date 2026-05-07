76bb9edb8e3f4aad8ac055a99fd996448d9e5060
Sai HU <laura.sai.hu@gmail.com>
2026-05-07 09:38:17 +0000

    create file

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module defines a Square."""
+
+
+class Square:
+"""Defines a square by size."""
+
+def __init__(self,size=0):
+    """Initialize the square."""
+
+
+    if not isinstance(size, int):
+    raise TypeError("size must be an integer")
+    if size < 0:
+    raise ValueError"size must be >=0"
+
+
+    self.__size = size
