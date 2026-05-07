7edb04d26fdefc688c4ecb2e03edd8b899deca7a
Sai HU <laura.sai.hu@gmail.com>
2026-05-07 13:49:42 +0000

    relocate conditions contrary to the setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,19 +5,9 @@
 class Square:
     """Defines a square by size."""
 
-    def __init__(self, size="0"):
+    def __init__(self, size=0):
         """Initialize the square."""
         self.size = size
-
-        @property
-        def size(self):
-            """Retrieve the size."""
-
-            return self.__size
-
-        @size.setter
-        def size(self, size):
-            """Retrieving the size."""
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
@@ -25,6 +15,17 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    @property
+    def size(self):
+        """Retrieve the size."""
+
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """Retrieving the size."""
+
     def area(self):
-        area = size ** 2
+        """Return the current square area."""
+        area = self.__size ** 2
         return area
