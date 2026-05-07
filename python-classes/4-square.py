b65f4e39540bf29276f262eff56a5ebe40bb2c6e
Sai HU <laura.sai.hu@gmail.com>
2026-05-07 11:10:20 +0000

    define area

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,14 +7,14 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize the square."""
-        self.size = size
+        self.__size = size
 
         @property
         def size(self):
             """Retrieve the size."""
 
             return self.__size
-        
+
         @size.setter
         def size(self, value):
             """Set the size."""
@@ -26,6 +26,5 @@ class Square:
         self.__size = value
 
     def area(self):
-        """Return the current square area."""
-
-        return self.__size ** 2
+        area = self.__size ** 2
+        return area
