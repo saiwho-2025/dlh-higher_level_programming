c5f23f28b8581699bebf08edb4a0744df3ea6b7b
Sai HU <laura.sai.hu@gmail.com>
2026-05-07 10:33:34 +0000

    create square area exercise

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module defines a Square."""
+
+
+class Square:
+    """Defines a square by size."""
+
+    def __init__(self, size=0):
+        """Initialize the square."""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """define a public attribute of the square."""
+
+        return self.__size ** 2
