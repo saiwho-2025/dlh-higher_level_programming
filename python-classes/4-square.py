bb00fdb6e66f57336f38057df9d010df5b28a19b
Sai HU <laura.sai.hu@gmail.com>
2026-05-07 10:52:36 +0000

    create the file

---
+++ python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module defines a Square."""
+
+
+class Square:
+    """Defines a square by size."""
+
+    def __init__(self, size=0):
+        """Initialize the square."""
+        self.size = size
+
+        @property
+        def size(self):
+            """Retrieve the size."""
+
+            return self.__size
+        
+        @size.setter
+        def size(self, value):
+            """Set the size."""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current square area."""
+
+        return self.__size ** 2
