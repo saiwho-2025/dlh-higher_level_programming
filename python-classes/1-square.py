ed6ad2c36e6674a474547a0389bbd8416a99fdb3
Sai HU <laura.sai.hu@gmail.com>
2026-05-07 06:37:58 +0000

    create task1

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module defines a class Square with attribute size"
+
+class Square:
+    """An empty class that defines a square."""
+pass
+
+
+    def_init_(self, new_size=None):
+        self.new_size = True
+        if new_size is not None:
+            self.size = new_size
