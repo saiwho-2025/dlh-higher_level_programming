685e6d09d80f2406acb6a89afc73798f7516d91e
root <laura.sai.hu@gmail.com>
2026-05-09 18:19:51 +0000

    Add to_json_string function

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module that contains a function that returns the JSON 
+Module that contains a function that returns the JSON
 representation of an object.
 """
 import json
