71bbac1c12dde7a9ed82891c46926446a864e7c3
root <laura.sai.hu@gmail.com>
2026-05-09 11:04:19 +0000

    add display

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""Rectangle class."""
+
+
+class Rectangle:
+    """Defines a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize rectangle."""
+        # We use the setters here to ensure validation logic runs
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set width."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area calculation."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the perimeter calculation."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+    
+    def __str__(self):
+        """Return a string representation of the rectangle using #."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        # Create a list of strings (rows), then join them with newlines
+        rect_rows = ["#" * self.__width for _ in range(self.__height)]
+        return "\n".join(rect_rows)
+
+    def __repr__(self):
+        """Return a string representation that can recreate the instance."""
+        return f"Rectangle({self.__width}, {self.__height})"
