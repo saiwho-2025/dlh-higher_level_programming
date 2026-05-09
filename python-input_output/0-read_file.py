17bc791adc5b0d10fcca41482f9f7f028077642d
root <laura.sai.hu@gmail.com>
2026-05-09 17:46:36 +0000

    creation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""
+Module that contains a function that reads a text file and prints it to stdout.
+"""
+
+
 def read_file(filename=""):
     """Reads a UTF8 text file and prints it to stdout."""
-    with open(filename, mode="r", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
