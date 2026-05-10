34ec29e3cd02d408cce404c8be752e57e8e2c43e
root <laura.sai.hu@gmail.com>
2026-05-10 18:38:03 +0000

    rewrite public attribute

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -3,10 +3,11 @@
 
 
 class Square:
-    """Defines a square by size."""
+    """Defines a square by size and position."""
     def __init__(self, size=0):
         """Initialize the square."""
         self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -22,6 +23,25 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
+    @property
+    def position(self):
+        """Gets the position of the square."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets the position of the square."""
+        if (
+            type(value) is not tuple or
+            len(value) != 2 or
+            type(value[0]) is not int or
+            type(value[1]) is not int or
+            value[0] < 0 or
+            value[1] < 0
+        ):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """Return the current square area."""
@@ -39,5 +59,5 @@ class Square:
         for i in range(self.__position[1]):
             print()
 
-        for i in range(self.__size)：
-        print(" " * self.__position[0] + "#" * self.__size)
+        for i in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
